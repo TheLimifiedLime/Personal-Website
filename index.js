b7b409506abce126b5e58db7ac06459ff9781de9
TheLimifiedCode <replit@thelimifiedlime.anonaddy.com>
2020-10-17 17:55:37 +0000

    Added robots.txt

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static(`public`))
+
 app.get('/', (req, res) => {
   res.sendFile(`/public/index.html`, {root: __dirname})
 })
 
-app.use(express.static(`public`))
+app.use(function (req, res, next) {
+  res.status(404).sendFile(`/public/404.html`, {root: __dirname})
+})
 
 app.listen(port, () => {
   console.log(`Started server on port ${port}`)
