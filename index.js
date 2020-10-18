693b8353aa33917a4193f2196827ca507913901b
TheLimifiedCode <replit@thelimifiedlime.anonaddy.com>
2020-10-18 04:33:58 +0000

    Updated a lot of stuff and I'm too lazy to detail this commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static(`public`))
+app.use(express.static(`public`) , {'extensions': ['html']})
 
 app.get('/', (req, res) => {
   res.sendFile(`/public/index.html`, {root: __dirname})
