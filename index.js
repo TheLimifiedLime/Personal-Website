6eaec69377f8cd50fcc1a8d29998d74f8be0cd6b
TheLimifiedCode <replit@thelimifiedlime.anonaddy.com>
2020-12-01 23:07:01 +0000

    Nvm going back to express

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-const fastify = require('fastify')({ logger: false})
-const path = require('path')
-const fs = require('fs')
+const express = require('express');
+const app = express();
 const port = 3000;
 
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'public'),
-  prefix: '/', // optional: default '/'
+app.use(express.static(`public`, {'extensions': ['html']} ))
+
+app.get('/', (req, res) => {
+  res.sendFile(`/public/index.html`, {root: __dirname})
 })
 
-fastify.setNotFoundHandler(function (req, reply) {
-  const stream = fs.createReadStream(`./public/404.html`)
-  reply.type(`text/html`).send(stream)
+app.use(function (req, res, next) {
+  res.status(404).sendFile(`/public/404.html`, {root: __dirname})
 })
 
-fastify.get('/', (req, reply) => {
-  const stream = fs.createReadStream(`./public/index.html`)
-  reply.type(`text/html`).send(stream)
-})
-
-fastify.listen(port, '0.0.0.0', (err) => {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-  fastify.log.info(`Server Started`)
+app.listen(port, () => {
+  console.log(`Started server on port ${port}`)
 })
