72e89cb591873732a297178d31b2c443caf50299
TheLimifiedLime <thelimifiedlime@protonmail.com>
2020-12-20 22:07:33 +0000

    Update V2

---
+++ public/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['*.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      green: colors.emerald
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
