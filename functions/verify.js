e624e41605f01eff87dbb175742382488aef2ae1
TheLimifiedLime <thelimifiedlime@protonmail.com>
2020-12-21 19:10:45 +0000

    working

--- functions/verify.js
+++ functions/verify.js
@@ -1,21 +1,25 @@
 module.exports = {
-  verify(token) {
-    const axios = require('axios');
+  async verify(token) {
+  const axios = require('axios');
     if (!token) {
       throw `variable 'token' is missing`
     }
-   const params = new URLSearchParams();
-   params.append('response', token)
-   params.append('secret', process.env.HCAPTCHA_SECRET)
-   params.append('sitekey', 'd957bbd0-ca4d-41c2-bfa7-cbc2b72c0305')
 
-    axios.post(`https://hcaptcha.com/siteverify`, params)
+    let returned;
+    const params = await new URLSearchParams();
+    await params.append('response', token)
+    await params.append('secret', process.env.HCAPTCHA_SECRET)
+    await params.append('sitekey', 'd957bbd0-ca4d-41c2-bfa7-cbc2b72c0305')
+
+    await axios.post(`https://hcaptcha.com/siteverify`, params)
       .then(function (data) {
-        console.log(data.data)
+        //console.log(data.data)
+        returned = data.data.success
       })
       .catch(function (error) {
         console.log(error)
       }) 
 
+      return returned;
   } 
 }
--- index.js
+++ index.js
@@ -6,71 +6,78 @@ const app = express();
 const port = 3000;
 
 app.use('/api/form', ratelimit({
-    burst: 1,  // Max 10 concurrent requests (if tokens)
-    rate: 0.10,  // Steady state: 1 request / 2 seconds
-    ip: true
+  burst: 1,  // Max 10 concurrent requests (if tokens)
+  rate: 0.10,  // Steady state: 1 request / 2 seconds
+  ip: true
 }));
 
-app.use(express.static(`public`, {'extensions': ['html']} ))
+app.use(express.static(`public`, { 'extensions': ['html'] }))
 
 app.use(express.urlencoded({
   extended: true
 }))
 
 app.get('/', (req, res) => {
-  res.sendFile(`/public/index.html`, {root: __dirname})
+  res.sendFile(`/public/index.html`, { root: __dirname })
 })
 
 app.post('/api/form', [
-  body('email').isLength({ min: 5, max: 50}).isEmail().normalizeEmail(),
-  body('subject').isLength({ min: 40, max: 200}).not().isEmpty().trim().escape(),
-  body('message').isLength({ min: 60 , max: 400}).not().isEmpty().trim().escape()
+  body('email').isLength({ min: 5, max: 50 }).isEmail().normalizeEmail(),
+  body('subject').isLength({ min: 40, max: 200 }).not().isEmpty().trim().escape(),
+  body('message').isLength({ min: 60, max: 400 }).not().isEmpty().trim().escape()
 ], (req, res) => {
-  
+
   // Finds the validation errors in this request and wraps them in an object with handy functions
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     res.send(`There was a error submitting the form and we couldnt redirect you` + JSON.stringify(errors))
     return res.redirect('https://issai.club/error');
   }
-  
-  const verifier = require(`./functions/verify.js`)
-  verifier.verify(req.body["h-captcha-response"])
- //res.redirect(`https://issai.club/success`) 
- const data = {
-  "content": "📨  New message on issai.club <@738966191519039640>!",
-  "embeds": [
-    {
-      "color": 3066993,
-      "fields": [
-        {
-          "name": "Email",
-          "value": req.body.email
-        },
-        {
-          "name": "Subject",
-          "value": req.body.subject
-        },
-        {
-          "name": "Message",
-          "value": req.body.message
-        }
-      ]
-    }
-  ]
-}
-  /* axios.post(process.env.WEBHOOK, data)
+
+  async function run() {
+    const verifier = require(`./functions/verify.js`)
+    const result = await verifier.verify(req.body["h-captcha-response"])
+    console.log(result)
+    return result;
+  }
+
+  run()
+
+  res.redirect(`https://issai.club/success`) 
+  const data = {
+    "content": "📨  New message on issai.club <@738966191519039640>!",
+    "embeds": [
+      {
+        "color": 3066993,
+        "fields": [
+          {
+            "name": "Email",
+            "value": req.body.email
+          },
+          {
+            "name": "Subject",
+            "value": req.body.subject
+          },
+          {
+            "name": "Message",
+            "value": req.body.message
+          }
+        ]
+      }
+    ]
+  }
+  axios.post(process.env.WEBHOOK, data)
   .then(function (response) {
     console.log(`Posted to webhook`)
   })
   .catch(function (error) {
     console.log(`Error!`+ error)
-  }) */
+  })
 });
 
 
-app.use(function (req, res, next) {
-  res.status(404).sendFile(`/public/404.html`, {root: __dirname})
+app.use(function(req, res, next) {
+  res.status(404).sendFile(`/public/404.html`, { root: __dirname })
 })
 
 app.listen(port, () => {
