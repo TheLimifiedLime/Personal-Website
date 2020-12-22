7ccfcc443f86d739b20bcd76cd6333a47e91be1f
TheLimifiedLime <thelimifiedlime@protonmail.com>
2020-12-22 01:17:35 +0000

    Tree shaked the css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   purge: {
     enabled: true,
-    content: ['*.html'],
+    content: ['./public/*.html'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      green: colors.emerald
-    },
     extend: {},
   },
   variants: {
