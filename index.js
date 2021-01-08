cc14d21955ddfbf79008ae42a0306a27ff4320f0
TheLimifiedLime <thelimifiedlime@protonmail.com>
2021-01-08 17:12:43 +0000

    Up ratelimit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(
   "/",
   ratelimit({
     burst: 3, // Max 10 concurrent requests (if tokens)
-    rate: 2, // Steady state: 1 request / 2 seconds
+    rate: 3, // Steady state: 1 request / 2 seconds
     ip: true,
   })
 );
