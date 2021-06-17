2057259db34938c95158f09e20d17f1df43c6696
TheLimifiedLime <69121493+TheLimifiedLime@users.noreply.github.com>
2021-06-17 01:09:26 +0000

    Stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,4 @@ module.exports = {
   mode: "jit",
   purge: ["./public/*.html", "./public/js/*.js"],
   darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 };
